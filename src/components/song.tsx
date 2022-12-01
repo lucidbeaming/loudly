fd9a96bb677b51d4f06b7c1218a5a149c704408a
Joshua Curry <zenphoto@gmail.com>
2022-12-01 00:51:03 +0000

    Beginning of component structure and API integration

---
+++ src/components/song.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import SoundPlayer from '../components/audio'
+
+function SongBlock(props) {
+}
+
+export default SongBlock
---
+++ src/components/trendingsongs.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Stack from 'react-bootstrap/Stack'
+import { GetSongs, Song } from '../data/api'
+import SongBlock from '../components/song'
+
+function TrendingSongs() {
+  const [playing, setPlaying] = useState<boolean>(false)
+  const [songs, setSongs] = useState<Song[]>([])
+
+  useEffect(() => {
+    if (songs.length == 0) {
+      GetSongs().then(res => {
+        setSongs(res)
+      })
+    }
+  }, [songs, setSongs])
+
+  return (
+    <div>
+      <h2>Trending songs</h2>
+      <Stack gap={3}>
+        {songs.map((song, index) => (
+          <Row key={index}>
+            <SongBlock />
+          </Row>
+        ))}
+      </Stack>
+    </div>
+  )
+}
+
+export default TrendingSongs
