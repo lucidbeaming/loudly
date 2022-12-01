19c85eed85f0773910c959be3da77c9a581e1d55
Joshua Curry <zenphoto@gmail.com>
2022-12-01 00:36:12 +0000

    Added types for song data

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,3 +1,59 @@
+interface Genre {
+  id: number,
+  name: string
+}
+
+interface Artist {
+  id: string,
+  name: string,
+  artist_name?: string,
+  muted?: boolean,
+  place?: string,
+  country?: string,
+  followers?: number,
+  follows?: number,
+  liked?: number,
+  releases?: number,
+  favorites?: number,
+  description?: string,
+  profile_image_path?: string,
+  cover_image_path?: string,
+  favorite_genres?: Genre[],
+  created?: Date,
+  location?: string,
+  social_accounts?: string[],
+  video_shout_out_count?: number,
+  external_songs_count?: number,
+  external_videos_count?: number,
+  crews?: string[]
+}
+
+export interface Song {
+  id: string,
+  name: string,
+  seo_name?: string,
+  artist: Artist,
+  artist_name?: string,
+  duration?: number,
+  bpm?: number,
+  likes: number,
+  faves?: number,
+  plays?: number,
+  reposts?: number,
+  comments?: number,
+  description?: string,
+  tags?: string[],
+  source_info?: string,
+  cover_image_path: string,
+  cover_image_aspect_ratio?: string,
+  music_file_path: string,
+  music_file_mimetype: string,
+  song_genres?: Genre[],
+  song_release?: Date,
+  mix_packs?: string[],
+  linked_crews?: string[]
+}
+
 export async function GetSongs(): Promise<Array<Song>> {
         const response = await fetch('https://api-stg.jam-community.com/song/trending')
         return await response.json()
