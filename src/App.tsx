68ac65b9dded79e3cffc9da9d53ee552934720a5
Joshua Curry <zenphoto@gmail.com>
2022-12-01 00:46:18 +0000

    Streamlined the basic app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import { GetSongs, LikeSong } from './data/api'
+import TrendingSongs from './components/trendingsongs'
+import './styles/styles.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <Container className="App">
+      <header className="App-header">
+        <h1>Loudly technical task</h1>
+      </header>
+      
+      <TrendingSongs />
+
+    </Container>
+  )
 }
 
-export default App;
+export default App
